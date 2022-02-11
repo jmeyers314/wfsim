eebe7a88a2867212027598cc9774c91c7411cadd
Josh Meyers <jmeyers314@gmail.com>
2022-02-11 10:40:54 -0800

    First attempt at ts_phosim-like perturbations

--- wfsim/__init__.py
+++ wfsim/__init__.py
@@ -4,3 +4,7 @@ from .atm import make_atmosphere
 from .utils import BBSED
 from .sim import SimpleSimulator
 from .catalog import MockStarCatalog
+from .sst import SSTFactory
+
+import os
+datadir = os.path.join(os.path.dirname(__file__), "data")
